74ab955bfa733aca925e9e4cd74af508dc485dc7
RawJrk <rodjer7777@gmail.com>
2023-07-17 14:45:48 +0300

    rebuilt lib/ scripts with TS

---
+++ lib/rand.ts
@@ -0,0 +1,11 @@
+type RandomNumber = (max: number) => number;
+export const randomNumber: RandomNumber = (max) =>
+  Math.floor(Math.random() * max);
+
+type RandomBoolean = (probability: number) => boolean;
+export const randomBoolean: RandomBoolean = (probability = 50) =>
+  randomNumber(100) < probability;
+
+type RandomChar = (...chars: string[]) => string;
+export const randomChar: RandomChar = (...chars) =>
+  chars[randomNumber(chars.length)];
