e22dbff592928700f9621d10f6eaf232aedb61b6
RawJrk <rodjer7777@gmail.com>
2023-10-17 15:39:45 +0300

    added custom delay for loading state

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,25 @@
+import { useState } from "react";
 import { useFetch } from "./hooks";
 
 function App() {
-  const [generate, data, loading, error] = useFetch("/api", { quotes: [null] });
+  const [run, data, loading, error] = useFetch("/api", { quotes: [null] });
   const [quote] = data.quotes;
+
+  const [waiting, setWaiting] = useState(false);
+
+  const generate = () => {
+    setWaiting(true);
+    run();
+
+    setTimeout(() => {
+      setWaiting(false);
+    }, 1000);
+  };
 
   return (
     <>
       <h2>Wilbur Says</h2>
-      {loading ? (
+      {loading || waiting ? (
         <p>🍌 [loading...]</p>
       ) : error ? (
         <p>🤷 [error]</p>
