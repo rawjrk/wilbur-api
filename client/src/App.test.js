58749cfca6d342bed07773be496cbe39251e3ad5
RawJrk <rodjer7777@gmail.com>
2023-10-17 15:20:42 +0300

    written down tests to do

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,8 +1,4 @@
 import { screen, render } from "@testing-library/react";
 import App from "./App";
 
-test("should have the right message in the dom", () => {
-  render(<App />);
-  const message = "Hi, stranger!";
-  expect(screen.getByText(message)).toBeInTheDocument();
-});
+test.todo("should have the right message in the dom");
---
+++ client/tests/unit/hooks.test.js
@@ -0,0 +1,7 @@
+describe("useFetch hook", () => {
+  test.todo("request to /api - successful");
+  test.todo("data updated upon request");
+  test.todo("loading updated to true, then back to false");
+  test.todo("request to /api/wrong updates error to true");
+  test.todo("error updates to false on new request");
+});
