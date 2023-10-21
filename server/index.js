a768d917e9424339425f702a86652a692d9cef0f
RawJrk <rodjer7777@gmail.com>
2023-10-21 20:43:43 +0300

    setup for deploy

--- server/index.js
+++ server/index.js
@@ -1,10 +1,17 @@
 const express = require("express");
 const app = express();
+const path = require("node:path");
 const PORT = process.env.PORT || 8081;
 
 const apiRoutes = require("./features/api/routes");
 
+app.use(express.static(path.resolve(__dirname, "../client/dist")));
+
 app.use("/api", apiRoutes);
+
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "../client/dist/index.html"));
+});
 
 app.listen(PORT, () => {
   console.log(`listening on port ${PORT}`);
